b4773dff738cea2bb6e5bc789b331a263d183324
Sophia <ms.shb2004@mail.ru>
2025-02-08 23:02:50 +0300

    feat: Add the `database_implementation` function

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from urls import app
+from base import database_implementation
 import logging
+import asyncio
 
 
 HOST = "127.0.0.1"
@@ -9,4 +11,5 @@ PORT = 8000
 if __name__ == '__main__':
     import uvicorn
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:    %(message)s')
+    asyncio.run(database_implementation())
     uvicorn.run(app, host=HOST, port=PORT)
