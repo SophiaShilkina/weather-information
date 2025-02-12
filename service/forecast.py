fe562a95acc8ee1f3f98e033c6b152829327b0ad
Sophia <ms.shb2004@mail.ru>
2025-02-12 20:59:49 +0300

    refactor: Rewrite the program code for sqlalchemy

--- service/forecast.py
+++ service/forecast.py
@@ -1,21 +1,23 @@
-import aiosqlite
 import logging
+from base import async_session
+from sqlalchemy import select
+from models import CitiesBase
+from fastapi import HTTPException
+
 
 # 3.    Метод возвращает список городов, для которых доступен прогноз погоды для определенного пользователя.
 
 
-async def cities_with_forecast(usid: int):
-    async with aiosqlite.connect("../cities.db") as db:
-        async with db.execute("SELECT city FROM cities WHERE id_user = ?", (usid,)) as cursor:
-            cities_tpl = await cursor.fetchall()
+async def cities_with_forecast(usid: int) -> list:
+    async with async_session() as session:
+        stmt = select(CitiesBase.city).where(CitiesBase.user_id == usid)
+        result = await session.execute(stmt)
 
-            if not cities_tpl:
-                logging.info("Cities not found.")
-                return {
-                    "Error": "Cities not found."
-                }
+        cities_list = result.scalars().all()
 
-            cities_list = [city[0] for city in cities_tpl]
+        if not cities_list:
+            logging.info("Cities not found.")
+            raise HTTPException(status_code=404, detail="Cities not found.")
 
-            logging.info(f"Cities: {cities_list}.")
-            return cities_list
+        logging.info(f"Cities: {cities_list}.")
+        return cities_list
